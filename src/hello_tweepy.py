5ddc352af653d228f0cde5fe990b174dcc73b2ff
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-14 19:25:34 -0400

    Obtained media urls of images and saved to list

--- src/hello_tweepy.py
+++ src/hello_tweepy.py
@@ -12,8 +12,11 @@ media_urls = []
 for tweet in public_tweets:
     #get json(its in unicode) from status and access as dictionary
     json_dict = tweet._json
-    #reading the media object from the entities
-    media_list = json_dict.get("entities").get("media")
+    #reading the media object from the entities and extended entities object
+    if(json_dict.get("entities").get("media")!=None):
+        media_list = json_dict.get("entities").get("media")
+    elif(json_dict.get("extended_entities")!=None):
+        media_list = json_dict.get("extended_entities").get("media")
     #iterating through the media object to look for photos and add media_url to list
     for media in media_list:
         if(media.get("type") == "photo"):
