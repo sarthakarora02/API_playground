f346b20f64969990bce8de8f451788f25fbfd726
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-14 22:32:25 -0400

    Downloading a test image from the url into directory made

--- src/hello_tweepy.py
+++ src/hello_tweepy.py
@@ -3,7 +3,7 @@ import twitter_credentials
 import json
 import os
 import urllib
-
+import shutil
 
 auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials.consumer_secret)
 auth.set_access_token(twitter_credentials.access_token, twitter_credentials.access_secret)
@@ -16,6 +16,7 @@ for tweet in public_tweets:
     #get json(its in unicode) from status and access as dictionary
     json_dict = tweet._json
     #reading the media object from the entities and extended entities object
+    media_list=[]
     if(json_dict.get("entities").get("media")!=None):
         media_list = json_dict.get("entities").get("media")
     elif(json_dict.get("extended_entities")!=None):
@@ -31,6 +32,14 @@ if(len(media_urls)>0):
 #New path for downloading images to
 curr_path = os.getcwd()
 new_path = curr_path + '/twitter_images'
-if not path os.path.exists(curr_path+"/twitter_images"):
+path_made = False
+if not os.path.exists(curr_path+"/twitter_images"):
     print "Creating path for images\n"
-       os.makedirs(new_path)
+    os.makedirs(new_path)
+    path_made = True
+else:
+    print "Path already exists. Please Delete before proceeding\n"
+
+if(path_made):
+    os.chdir(new_path)
+    urllib.urlretrieve("http://www.gunnerkrigg.com//comics/00000001.jpg", "image0001.jpg")
