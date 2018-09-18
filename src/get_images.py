52fdb39ce9382832950e8d7ad2eb8e15bc15c065
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-17 23:03:16 -0400

    Checking if no statuses received from twitter

--- src/get_images.py
+++ src/get_images.py
@@ -14,46 +14,52 @@ try:
     public_tweets = api.home_timeline()
 except:
     print "Tweets couldn't be obtained"
+
 media_urls = []
-for tweet in public_tweets:
-    #get json(its in unicode) from status and access as dictionary
-    json_dict = tweet._json
-    #reading the media object from the entities and extended entities object
-    media_list=[]
-    #TODO improve this logic as its not getting all images and skipping tweets I think
-    if(json_dict.get("entities").get("media")!=None):
-        media_list = json_dict.get("entities").get("media")
-    elif(json_dict.get("extended_entities")!=None):
-        media_list = json_dict.get("extended_entities").get("media")
-    #iterating through the media object to look for photos and add media_url to list
-    for media in media_list:
-        if(media.get("type") == "photo"):
-            media_urls.append(media.get("media_url"))
 
-if(len(media_urls)>0):
-    print("Images received from Twitter.\n")
+if(len(public_tweets)>0):
+    for tweet in public_tweets:
+        #get json(its in unicode) from status and access as dictionary
+        json_dict = tweet._json
+        #reading the media object from the entities and extended entities object
+        media_list=[]
+        #TODO improve this logic as its not getting all images and skipping tweets I think
+        if(json_dict.get("entities").get("media")!=None):
+            media_list = json_dict.get("entities").get("media")
+        elif(json_dict.get("extended_entities")!=None):
+            media_list = json_dict.get("extended_entities").get("media")
+        #iterating through the media object to look for photos and add media_url to list
+        for media in media_list:
+            if(media.get("type") == "photo"):
+                media_urls.append(media.get("media_url"))
+
+    if(len(media_urls)>0):
+        print("Images received from Twitter.\n")
+    else:
+            print("No images received. Try again.\n")
+
+    #New path for downloading images to
+    curr_path = os.getcwd()
+    new_path = curr_path + '/twitter_images'
+    path_made = False
+    if not os.path.exists(curr_path+"/twitter_images"):
+        print "Creating path for images\n"
+        os.makedirs(new_path)
+        path_made = True
+    else:
+        print "Path already exists. Please Delete before proceeding\n"
+
+    #data_%03i.dat" % i
+    #filename = 'data_%d.dat'%(i,)
+
+    #If required path is made. Download images from media_urls list
+    if(path_made):
+        os.chdir(new_path)
+        i=0
+        print "Downloading Twitter images"
+        for img in media_urls:
+            urllib.urlretrieve(img, "img_%03i.jpg"%i)
+            i=i+1
+
 else:
-        print("No images received. Try again.\n")
-
-#New path for downloading images to
-curr_path = os.getcwd()
-new_path = curr_path + '/twitter_images'
-path_made = False
-if not os.path.exists(curr_path+"/twitter_images"):
-    print "Creating path for images\n"
-    os.makedirs(new_path)
-    path_made = True
-else:
-    print "Path already exists. Please Delete before proceeding\n"
-
-#data_%03i.dat" % i
-#filename = 'data_%d.dat'%(i,)
-
-#If required path is made. Download images from media_urls list
-if(path_made):
-    os.chdir(new_path)
-    i=0
-    print "Downloading Twitter images"
-    for img in media_urls:
-        urllib.urlretrieve(img, "img_%03i.jpg"%i)
-        i=i+1
+    print "No tweets in Home Timeline"
