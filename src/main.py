a90e265a06c8e22687ffdbfca9afa2fdf16243aa
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 00:51:06 -0400

    Calling Video analysis script with path passed

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ import make_video
 
 if __name__ == "__main__":
     img_path = get_images.read_twitter()
-    print("OUTPUT " + str(ans))
+    print("OUTPUT " + str(img_path))
     if(img_path!=0):
         print "Twitter images downloaded ./twitter_images"
-        ans = make_video.make_vid(img_path)
-        print "Video O/p: "+str(ans)
+        val = make_video.make_vid(img_path)
+        if (val==1):
+            ans = analyse_images_video.analyse(img_path)
+        else:
     else:
-        print "Error occurred"
+        print "Error occurred in fetching data from Twitter"
