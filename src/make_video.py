7f7baec39b48ad582bb86b5b5d2b1ea546704d2b
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 00:34:14 -0400

    Handling path from main in make_video.py

--- src/make_video.py
+++ src/make_video.py
@@ -1,9 +1,12 @@
 import subprocess
+import os
 
-#for command line
-#ffmpeg -r 1 -i img_%03d.jpg -vcodec mpeg4 -y movie.mp4
-try:
-    code=subprocess.call(['ffmpeg', '-r', '1', '-i', 'img_%03d.jpg', '-vcodec', 'mpeg4', '-y', 'vid.mp4'])
-    print ('Video Process: ',code,'\n')
-except Exception as e:
-    print "Error occurred in Video formation. "+str(e)
+def make_vid(path):
+    #for command line
+    #ffmpeg -r 1 -i img_%03d.jpg -vcodec mpeg4 -y movie.mp4
+    os.chdir(path)
+    try:
+        code=subprocess.call(['ffmpeg', '-r', '1', '-i', 'img_%03d.jpg', '-vcodec', 'mpeg4', '-y', 'vid.mp4'])
+        print ('Video Process: ',code,'\n')
+    except Exception as e:
+        print "Error occurred in Video formation. "+str(e)
