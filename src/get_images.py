f10917f455f3ab6f14ebd4499c7815a20a9c51b1
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-17 22:34:41 -0400

    Shifting code for retrieving images to different script

---
+++ src/get_images.py
@@ -0,0 +1,54 @@
+import tweepy
+import twitter_credentials
+import json
+import os
+import urllib
+import shutil
+
+auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials.consumer_secret)
+auth.set_access_token(twitter_credentials.access_token, twitter_credentials.access_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+media_urls = []
+for tweet in public_tweets:
+    #get json(its in unicode) from status and access as dictionary
+    json_dict = tweet._json
+    #reading the media object from the entities and extended entities object
+    media_list=[]
+    #TODO improve this logic as its not getting all images and skipping tweets I think
+    if(json_dict.get("entities").get("media")!=None):
+        media_list = json_dict.get("entities").get("media")
+    elif(json_dict.get("extended_entities")!=None):
+        media_list = json_dict.get("extended_entities").get("media")
+    #iterating through the media object to look for photos and add media_url to list
+    for media in media_list:
+        if(media.get("type") == "photo"):
+            media_urls.append(media.get("media_url"))
+
+if(len(media_urls)>0):
+    print("Images received from Twitter.\n")
+
+#New path for downloading images to
+curr_path = os.getcwd()
+new_path = curr_path + '/twitter_images'
+path_made = False
+if not os.path.exists(curr_path+"/twitter_images"):
+    print "Creating path for images\n"
+    os.makedirs(new_path)
+    path_made = True
+else:
+    print "Path already exists. Please Delete before proceeding\n"
+
+#data_%03i.dat" % i
+#filename = 'data_%d.dat'%(i,)
+
+#If required path is made. Download images from media_urls list
+if(path_made):
+    os.chdir(new_path)
+    i=0
+    print "Downloading Twitter images"
+    for img in media_urls:
+        urllib.urlretrieve(img, "img_%03i.jpg"%i)
+        i=i+1
