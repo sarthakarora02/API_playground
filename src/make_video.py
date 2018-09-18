8e9d8c493d236dfb8919e2ed2cf9ba59c7ff562f
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 00:38:01 -0400

    print messages and return values added

--- src/make_video.py
+++ src/make_video.py
@@ -8,5 +8,11 @@ def make_vid(path):
     try:
         code=subprocess.call(['ffmpeg', '-r', '1', '-i', 'img_%03d.jpg', '-vcodec', 'mpeg4', '-y', 'vid.mp4'])
         print ('Video Process: ',code,'\n')
+        if(code==0):
+            print "Video built successfully"
+            return 1
+        else:
+            return 0
     except Exception as e:
         print "Error occurred in Video formation. "+str(e)
+        return 0
