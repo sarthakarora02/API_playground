6198a7fcd62be6694167186f5b8427753e38bcd2
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 00:55:46 -0400

    Shifting Video analysis code to different script

---
+++ src/analyse_images_video.py
@@ -0,0 +1,38 @@
+import os
+from google.cloud import videointelligence
+
+def analyse (path):
+    os.chdir(path)
+    """Detect labels given a file path."""
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    features = [videointelligence.enums.Feature.LABEL_DETECTION]
+
+    with open('./vid.mp4', 'rb') as movie:
+        input_content = movie.read()
+
+    operation = video_client.annotate_video(
+        features=features, input_content=input_content)
+    print('\nProcessing video for label annotations:')
+
+    result = operation.result(timeout=90)
+    print('\nFinished processing.')
+
+    # Process video/segment level label annotations
+    segment_labels = result.annotation_results[0].segment_label_annotations
+    for i, segment_label in enumerate(segment_labels):
+        print('Video label description: {}'.format(
+        segment_label.entity.description))
+    for category_entity in segment_label.category_entities:
+        print('\tLabel category description: {}'.format(
+            category_entity.description))
+
+    for i, segment in enumerate(segment_label.segments):
+        start_time = (segment.segment.start_time_offset.seconds +
+                      segment.segment.start_time_offset.nanos / 1e9)
+        end_time = (segment.segment.end_time_offset.seconds +
+                    segment.segment.end_time_offset.nanos / 1e9)
+        positions = '{}s to {}s'.format(start_time, end_time)
+        confidence = segment.confidence
+        print('\tSegment {}: {}'.format(i, positions))
+        print('\tConfidence: {}'.format(confidence))
+        print('\n')
