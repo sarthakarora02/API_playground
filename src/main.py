13dd3644fac086775b99abb1297da257bab8ed21
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 00:28:37 -0400

    handling pa†h from get_images.py in main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import get_images
+import make_video
 
 if __name__ == "__main__":
-    ans = get_images.read_twitter()
+    img_path = get_images.read_twitter()
     print("OUTPUT " + str(ans))
+    if(img_path!=0):
+        print "Twitter images downloaded ./twitter_images"
+        ans = make_video.make_vid(img_path)
+        print "Video O/p: "+str(ans)
+    else:
+        print "Error occurred"
