e07cc15e3b6f290c5bcd60f4f9780eccc544d1eb
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-18 22:03:26 -0400

    Rephrasing print messages after different scripts

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import analyse_images_video
 
 if __name__ == "__main__":
     img_path = get_images.read_twitter()
-    print("OUTPUT " + str(img_path))
+    print("Path to images " + str(img_path))
     if(img_path!=0):
-        print "Twitter images downloaded ./twitter_images"
+        print "Twitter images downloaded to ./twitter_images"
         val = make_video.make_vid(img_path)
         if (val==1):
             ans = analyse_images_video.analyse(img_path)
