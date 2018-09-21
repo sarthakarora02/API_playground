a49c7b63cbea3b14d8c9b9958ed7e95a0059ca3a
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-21 15:38:02 -0400

    Adding twitter handle input facility in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import get_images
 import make_video
 import analyse_images_video
+import sys
 
 if __name__ == "__main__":
-    img_path = get_images.read_twitter()
+
+    twitter_handle = raw_input("Enter twitter handle: ")
+    img_path = get_images.read_twitter(twitter_handle)
     print("Path to images " + str(img_path))
     if(img_path!=0):
         print "Twitter images downloaded to ./twitter_images"
