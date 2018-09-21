f0406c1689dd23fa7712a99c336a41dea95d56b1
Sarthak Arora <sarthak.arora@gmail.com>
2018-09-21 18:45:48 -0400

    added facility for user to enter number of images

--- src/main.py
+++ src/main.py
@@ -6,7 +6,19 @@ import sys
 if __name__ == "__main__":
 
     twitter_handle = raw_input("Enter twitter handle: ")
-    img_path = get_images.read_twitter(twitter_handle)
+    image_num = raw_input("Enter the number of images you would like to use: ")
+    img_path = ""
+    try:
+        num = int(image_num)
+        if(num>0):
+            img_path = get_images.read_twitter(twitter_handle, image_num)
+        else:
+            print "Number <= 0. Please enter a positive integer"
+            exit()
+    except ValueError:
+       print("Please enter an integer")
+       exit()
+
     print("Path to images " + str(img_path))
     if(img_path!=0):
         print "Twitter images downloaded to ./twitter_images"
