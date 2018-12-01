3c74a69dfc3db1e7aab9096d621558ae159be0aa
Sarthak Arora <sarthak.arora@gmail.com>
2018-11-30 21:07:57 -0500

    add mysql for session table

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import get_images
 import make_video
 import analyse_images_video
 import sys
+import mysql_connection2
+import datetime
 
 if __name__ == "__main__":
 
@@ -30,4 +32,13 @@ if __name__ == "__main__":
     else:
         print ("Error occurred in fetching data from Twitter")
 
-    
+    mydb = mysql_connection2.connect()
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO session (twitter_handle, num_images, img_path, vid_path, login_time) VALUES (%s, %s, %s, %s, %s);"
+    val = (twitter_handle, int(image_num), "./twitter_images", "./twitter_images", str(datetime.datetime.now()))
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
